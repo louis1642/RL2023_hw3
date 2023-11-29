c1100f97a81a26a8ce9b1d23eb8eb22fabe6219b
stefanocovone <stefanocovone@gmail.com>
2023-11-29 19:14:16 +0100

    commit from my repo

--- opencv_ros/src/opencv_ros_node.cpp
+++ opencv_ros/src/opencv_ros_node.cpp
@@ -4,6 +4,7 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/opencv.hpp"
 
 static const std::string OPENCV_WINDOW = "Image window";
 
@@ -19,7 +20,7 @@ public:
     : it_(nh_)
   {
     // Subscribe to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/usb_cam/image_raw", 1,
+    image_sub_ = it_.subscribe("/iiwa/camera1/image_raw", 1,
       &ImageConverter::imageCb, this);
     image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
@@ -44,16 +45,70 @@ public:
       return;
     }
 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+    cv::Mat im;
+    cv::cvtColor(cv_ptr->image, im, cv::COLOR_BGR2GRAY);
+
+      // Setup SimpleBlobDetector parameters.
+  cv::SimpleBlobDetector::Params params;
+
+  // Change thresholds
+  params.minThreshold = 1;
+  params.maxThreshold = 250;
+  params.thresholdStep = 1;
+
+  params.filterByColor = true;
+  params.blobColor = 0;
+  params.minRepeatability = 4;
+  params.minDistBetweenBlobs = 1;
+
+  // Filter by Area.
+  params.filterByArea = true;
+  params.minArea = 100;
+  params.maxArea = 10000000;
+
+  // Filter by Circularity
+  params.filterByCircularity = true;
+  params.minCircularity = 0.7;
+
+  // Filter by Convexity
+  params.filterByConvexity = true;
+  params.minConvexity = 0.87;
+
+  // Filter by Inertia
+  params.filterByInertia = true;
+  params.minInertiaRatio = 0.4;
+
+
+  // Storage for blobs
+  std::vector<cv::KeyPoint> keypoints;
+
+  // // Set up detector with params
+  // cv::SimpleBlobDetector detector;
+
+  // // Detect blobs
+  // detector.detect( im, keypoints);
+
+  // Set up detector with params
+  cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);   
+
+  // Detect blobs
+  detector->detect( im, keypoints);
+
+  // Draw detected blobs as red circles.
+  // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
+  // the size of the circle corresponds to the size of blob
+
+  cv::Mat im_with_keypoints;
+  cv::drawKeypoints( im, keypoints, im_with_keypoints, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+
 
     // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
+    cv::imshow(OPENCV_WINDOW, im_with_keypoints);
+    cv::waitKey(1);
 
     // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
+    sensor_msgs::ImagePtr output_image_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", im_with_keypoints).toImageMsg();
+    image_pub_.publish(output_image_msg);
   }
 };
 
